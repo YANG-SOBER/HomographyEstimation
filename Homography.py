d699298d10ed415f91831938749e624159a17128
YANG-SOBER <88379706+YANG-SOBER@users.noreply.github.com>
2021-09-16 17:40:53 +0800

    Python3, edit small bugs

--- Homography.py
+++ Homography.py
@@ -1,52 +1,39 @@
-__author__ = 'jhughes'
 import cv2
 import numpy as np
-import getopt
-import sys
 import random
+import sys
+import getopt
 
-#
+
 # Read in an image file, errors out if we can't find the file
-#
 def readImage(filename):
     img = cv2.imread(filename, 0)
     if img is None:
-        print('Invalid image:' + filename)
-        return None
+        print('Invalid image' + filename)
     else:
         print('Image successfully read...')
-        return img
 
+    return img
 
 # This draws matches and optionally a set of inliers in a different color
-# Note: I lifted this drawing portion from stackoverflow and adjusted it to my needs because OpenCV 2.4.11 does not
-# include the drawMatches function
-def drawMatches(img1, kp1, img2, kp2, matches, inliers = None):
-    # Create a new output image that concatenates the two images together
-    rows1 = img1.shape[0]
-    cols1 = img1.shape[1]
-    rows2 = img2.shape[0]
-    cols2 = img2.shape[1]
+def drawMatches(img1, kp1, img2, kp2, matches, inliers=None):
+    row1 = img1.shape[0]
+    row2 = img2.shape[0]
+    col1 = img1.shape[1]
+    col2 = img2.shape[1]
 
-    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
+    out = np.zeros((max(row1, row2), col1 + col2, 3), dtype='uint8')
 
-    # Place the first image to the left
-    out[:rows1,:cols1,:] = np.dstack([img1, img1, img1])
+    out[:row1, :col1, :] = np.dstack((img1, img1, img1))
 
-    # Place the next image to the right of it
-    out[:rows2,cols1:cols1+cols2,:] = np.dstack([img2, img2, img2])
+    out[:row2, col1:col1+col2, :] = np.dstack((img2, img2, img2))
 
-    # For each pair of points we have between both images
-    # draw circles, then connect a line between them
-    for mat in matches:
+    for match in matches:
+        img1_idx = match.queryIdx # index of the descriptor in query descriptors
+        img2_idx = match.trainIdx # index of the descriptor in train descriptors
 
-        # Get the matching keypoints for each of the images
-        img1_idx = mat.queryIdx
-        img2_idx = mat.trainIdx
-
-        # x - columns, y - rows
-        (x1,y1) = kp1[img1_idx].pt
-        (x2,y2) = kp2[img2_idx].pt
+        (x1, y1) = kp1[img1_idx].pt
+        (x2, y2) = kp2[img2_idx].pt
 
         inlier = False
 
@@ -54,177 +41,183 @@ def drawMatches(img1, kp1, img2, kp2, matches, inliers = None):
             for i in inliers:
                 if i.item(0) == x1 and i.item(1) == y1 and i.item(2) == x2 and i.item(3) == y2:
                     inlier = True
-
         # Draw a small circle at both co-ordinates
         cv2.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)
-        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
+        cv2.circle(out, (int(x2)+col1,int(y2)), 4, (255, 0, 0), 1)
 
         # Draw a line in between the two points, draw inliers if we have them
         if inliers is not None and inlier:
-            cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (0, 255, 0), 1)
+            cv2.line(out, (int(x1),int(y1)), (int(x2)+col1,int(y2)), (0, 255, 0), 1)
         elif inliers is not None:
-            cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (0, 0, 255), 1)
+            cv2.line(out, (int(x1),int(y1)), (int(x2)+col1,int(y2)), (0, 0, 255), 1)
 
         if inliers is None:
-            cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255, 0, 0), 1)
+            cv2.line(out, (int(x1),int(y1)), (int(x2)+col1,int(y2)), (255, 0, 0), 1)
 
     return out
 
-#
-# Runs sift algorithm to find features
-#
+# Run SIFT algorithm to find features
 def findFeatures(img):
-    print("Finding Features...")
-    sift = cv2.SIFT()
-    keypoints, descriptors = sift.detectAndCompute(img, None)
+    # BGR to Gray
+    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Create SIFT detector
+    sift = cv2.xfeatures2d.SIFT_create()
 
-    img = cv2.drawKeypoints(img, keypoints)
-    cv2.imwrite('sift_keypoints.png', img)
+    # Compute keypoints and descriptors
+    keypoints, descriptors = sift.detectAndCompute(img, None) # List of keypoints, Numpy array of descriptors (Number of Keypoints, 128)
+
+    # Draw keypoints
+    img_kps = cv2.drawKeypoints(img, keypoints, outImage=None)
+
+    # Store the image with keypoints
+    cv2.imwrite("Image_with_SIFT_Keypoints.jpg", img_kps)
 
     return keypoints, descriptors
 
-#
 # Matches features given a list of keypoints, descriptors, and images
-#
-def matchFeatures(kp1, kp2, desc1, desc2, img1, img2):
-    print("Matching Features...")
-    matcher = cv2.BFMatcher(cv2.NORM_L2, True)
-    matches = matcher.match(desc1, desc2)
-    matchImg = drawMatches(img1,kp1,img2,kp2,matches)
-    cv2.imwrite('Matches.png', matchImg)
+def matchFeatures(kp1, kp2, des1, des2, img1, img2):
+    # Create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_L2, True)
+
+    # Match descriptors
+    matches = bf.match(des1, des2)
+
+    # Draw matches
+    img_match = drawMatches(img1, kp1, img2, kp2, matches)
+
+    # Store the image with matches
+    cv2.imwrite("Image_with_BFMatcher_Keypoints.jpg", img_match)
+
     return matches
 
-
-#
-# Computers a homography from 4-correspondences
-#
+# Compute a homography from 4-correspondences
 def calculateHomography(correspondences):
-    #loop through correspondences and create assemble matrix
-    aList = []
-    for corr in correspondences:
-        p1 = np.matrix([corr.item(0), corr.item(1), 1])
-        p2 = np.matrix([corr.item(2), corr.item(3), 1])
+    A_list = []
+    for corr in correspondences: # corr: np.array([x1, y1, x2, y2])
 
-        a2 = [0, 0, 0, -p2.item(2) * p1.item(0), -p2.item(2) * p1.item(1), -p2.item(2) * p1.item(2),
-              p2.item(1) * p1.item(0), p2.item(1) * p1.item(1), p2.item(1) * p1.item(2)]
-        a1 = [-p2.item(2) * p1.item(0), -p2.item(2) * p1.item(1), -p2.item(2) * p1.item(2), 0, 0, 0,
-              p2.item(0) * p1.item(0), p2.item(0) * p1.item(1), p2.item(0) * p1.item(2)]
-        aList.append(a1)
-        aList.append(a2)
+        # write each point correspondence as homogeneous coordinates
+        p1 = np.array([corr.item(0), corr.item(1), 1]) # [x1, y1, 1]
+        p2 = np.array([corr.item(2), corr.item(3), 1]) # [x2, y2, 1]
 
-    matrixA = np.matrix(aList)
+        # Construct Ai, s.t. Aih = 0, where Ai is (2, 9), h is (9, 1)
+        # A_1 = [0, 0, 0, -x_1, -y_1, -1, y_2*x_1, y_2*y_1, y_2]
+        A_1 = [0, 0, 0, -p1.item(0), -p1.item(1), -1, p2.item(1) * p1.item(0), p2.item(1) * p1.item(1), p2.item(1)]
+        # A_2 = [x1, y1, 1, 0, 0, 0, -x_2*x_1, -x_2*y_1, -x_2]
+        A_2 = [p1.item(0), p1.item(1), 1, 0, 0, 0, -p2.item(0) * p1.item(0), -p2.item(0) * p1.item(1), -p2.item(0)]
 
-    #svd composition
-    u, s, v = np.linalg.svd(matrixA)
+        A_list.append(A_1)
+        A_list.append(A_2)
+    # Construct A, s.t. Ah = 0, where A is (2* len(correspondences), 9)
+    A = np.array(A_list)
 
-    #reshape the min singular value into a 3 by 3 matrix
-    h = np.reshape(v[8], (3, 3))
+    # compute and transform h, to get homography matrix H
+    U, S, VT = np.linalg.svd(A)
+    V = VT.T
+    h = V[:, -1] # h is in the shape of (9, 1)
+    h /= h.item(8)
+    H = h.reshape((3, 3)) # or H = np.reshape(h, (3, 3))
 
-    #normalize and now we have h
-    h = (1/h.item(8)) * h
-    return h
+    return H
 
+# Calculate the geometric distance between estimated points and original points
+def geometricDistance(correspondence, H):
+    p1 = np.array([correspondence.item(0), correspondence.item(1), 1]) # p1: [x1, y1, 1] homogeneous coordinates
+    p2_esti = H @ p1 # p2 shape: (3,)
 
-#
-#Calculate the geometric distance between estimated points and original points
-#
-def geometricDistance(correspondence, h):
+    p2 = np.array([correspondence.item(2), correspondence.item(3), 1]) # p2: [x2, y2, 1] homogeneous coordinates
+    error = p2_esti - p2
+    geo_dist = np.linalg.norm(error)
+    return geo_dist
 
-    p1 = np.transpose(np.matrix([correspondence[0].item(0), correspondence[0].item(1), 1]))
-    estimatep2 = np.dot(h, p1)
-    estimatep2 = (1/estimatep2.item(2))*estimatep2
+# Run through ransac algorithm, create homographies from correspondences
+def ransac(corr, thresh): # thresh: inliers ratio threshold
 
-    p2 = np.transpose(np.matrix([correspondence[0].item(2), correspondence[0].item(3), 1]))
-    error = p2 - estimatep2
-    return np.linalg.norm(error)
-
-
-#
-#Runs through ransac algorithm, creating homographies from random correspondences
-#
-def ransac(corr, thresh):
+    dist_thresh = 5 # 5 pix
     maxInliers = []
-    finalH = None
-    for i in range(1000):
-        #find 4 random points to calculate a homography
+
+    # Repeat for 1000 trials
+    for trial in range(1000):
+        # *Sample* Select a random sample of 4 correspondences
         corr1 = corr[random.randrange(0, len(corr))]
         corr2 = corr[random.randrange(0, len(corr))]
-        randomFour = np.vstack((corr1, corr2))
+        corrFour = np.vstack((corr1, corr2))
         corr3 = corr[random.randrange(0, len(corr))]
-        randomFour = np.vstack((randomFour, corr3))
+        corrFour = np.vstack((corrFour, corr3))
         corr4 = corr[random.randrange(0, len(corr))]
-        randomFour = np.vstack((randomFour, corr4))
+        corrFour = np.vstack((corrFour, corr4))
 
-        #call the homography function on those points
-        h = calculateHomography(randomFour)
+        # *Compute* Compute the homography from the above 4 randomly sampled points
+        H = calculateHomography(corrFour)
         inliers = []
 
+        # *Calculate* Calculate the distance d for each putative correspondence
         for i in range(len(corr)):
-            d = geometricDistance(corr[i], h)
-            if d < 5:
-                inliers.append(corr[i])
+            d = geometricDistance(corr[i], H)
+            # Compute the number of inliers consistent with H by the number of correspondences for which d < dist_thresh
+            if d < dist_thresh:
+                inliers.append(corr[i]) # [array1, array2, ...]
 
         if len(inliers) > len(maxInliers):
             maxInliers = inliers
-            finalH = h
-        print "Corr size: ", len(corr), " NumInliers: ", len(inliers), "Max inliers: ", len(maxInliers)
+            finalH = H
+        print(f"Corr size: {len(corr)}, Number of Inliers: {len(inliers)}, NUmber of maxInliers: {len(maxInliers)}")
 
-        if len(maxInliers) > (len(corr)*thresh):
+        if len(maxInliers) > (len(corr) * thresh):
             break
     return finalH, maxInliers
 
-
-#
 # Main parses argument list and runs the functions
-#
 def main():
-    args, img_name = getopt.getopt(sys.argv[1:],'', ['threshold='])
-    args = dict(args)
+    opts, args = getopt.getopt(sys.argv[1:], '', ['threshold=']) # opts: [('threshold', '0.6')] args: ['img1.png', 'img2.png']
+    opts_dict = dict(opts)
 
-    estimation_thresh = args.get('--threshold')
-    print "Estimation Threshold: ", estimation_thresh
-    if estimation_thresh is None:
-        estimation_thresh = 0.60
+    # get the thresh from the command line
+    thresh = float(opts_dict.get('--threshold'))
 
-    img1name = str(img_name[0])
-    img2name = str(img_name[1])
-    print("Image 1 Name: " + img1name)
-    print("Image 2 Name: " + img2name)
+    # get the image path from the command line
+    img1_path = args[0]
+    img2_path = args[1]
 
-    #query image
-    img1 = readImage(img_name[0])
-    #train image
-    img2 = readImage(img_name[1])
+    # read the two images
+    img1 = readImage(img1_path)
+    img2 = readImage(img2_path)
 
-    #find features and keypoints
-    correspondenceList = []
-    if img1 is not None and img2 is not None:
-        kp1, desc1 = findFeatures(img1)
-        kp2, desc2 = findFeatures(img2)
-        print "Found keypoints in " + img1name + ": " + str(len(kp1))
-        print "Found keypoints in " + img2name + ": " + str(len(kp2))
-        keypoints = [kp1,kp2]
-        matches = matchFeatures(kp1, kp2, desc1, desc2, img1, img2)
-        for match in matches:
-            (x1, y1) = keypoints[0][match.queryIdx].pt
-            (x2, y2) = keypoints[1][match.trainIdx].pt
-            correspondenceList.append([x1, y1, x2, y2])
+    # find keypoints and descriptors for two images
+    kp1, des1 = findFeatures(img1)
+    kp2, des2 = findFeatures(img2)
 
-        corrs = np.matrix(correspondenceList)
+    # Match features
+    matches = matchFeatures(kp1, kp2, des1, des2, img1, img2)
 
-        #run ransac algorithm
-        finalH, inliers = ransac(corrs, estimation_thresh)
-        print "Final homography: ", finalH
-        print "Final inliers count: ", len(inliers)
+    # Matrix of point correspondences
+    '''
+    np.array([[x1, y1, x2, y2],
+              [x1, y1, x2, y2],
+              ...])
+    '''
+    corrs_list = []
+    for match in matches:
+        img1_idx = match.queryIdx # index of descriptor in query descriptors
+        img2_idx = match.trainIdx # index of descriptor in train descriptors
 
-        matchImg = drawMatches(img1,kp1,img2,kp2,matches,inliers)
-        cv2.imwrite('InlierMatches.png', matchImg)
+        (x1, y1) = kp1[img1_idx].pt
+        (x2, y2) = kp2[img2_idx].pt
+        corr = [x1, y1, x2, y2]
+        corrs_list.append(corr)
 
-        f = open('homography.txt', 'w')
-        f.write("Final homography: \n" + str(finalH)+"\n")
-        f.write("Final inliers count: " + str(len(inliers)))
-        f.close()
+    corrs = np.array(corrs_list)
 
+    # Run RANdom SAmple Consensus algorithm
+    finalH, inliers = ransac(corrs, thresh)
+    print(f"Final Homography: {finalH}")
+    print(f"Final inliers count: ", len(inliers))
 
-if __name__ == "__main__":
+    matchImg = drawMatches(img1, kp1, img2, kp2, matches, inliers)
+    cv2.imwrite("InlierMatches.png", matchImg)
+
+    with open("homography.txt", 'w') as f:
+        f.write(f"Final homography: \n {finalH} \n")
+        f.write(f"Final inliers count: {len(inliers)}")
+
+if __name__ == '__main__':
     main()
